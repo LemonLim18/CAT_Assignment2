61cb97a5a4922b65675edeb1ea759c6b798edb95
LemonLim18 <limmiinning@gmail.com>
2024-01-07 11:53:58 +0800

    Upload video and add in rise animation

--- src/components/introVideo.js
+++ src/components/introVideo.js
@@ -3,7 +3,7 @@ import "../components/intro_video.css";
 
 const IntroVideo = () => {
     return (
-        <section className="video-section">
+        <section className="video-section rise">
             <iframe title="Intro Video" src="https://www.youtube.com/embed/RBAHYHsePtE" frameborder="0" allow />
         </section> 
     );
